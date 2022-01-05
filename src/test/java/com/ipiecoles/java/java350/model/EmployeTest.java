4b6bc70dc14da097ac6d3b0f5366bc013e49903a
GOKOL Semi <gokol.semih@gmail.com>
2022-01-05 15:14:46 +0100

    TU employe avec les 4 cas

--- src/test/java/com/ipiecoles/java/java350/model/EmployeTest.java
+++ src/test/java/com/ipiecoles/java/java350/model/EmployeTest.java
@@ -7,16 +7,59 @@ import java.time.LocalDate;
 
 public class EmployeTest {
 
+    //cas 1 : DateNow = 05/01/2022 | dateEmbauche = 05/01/2023 | nbAnneeAnciennete : 0
     @Test
-    public void testGetNombreAnneeAncienneteWhenDateEmbaucheIsNull(){
+    public void testGetNombreAnneeAncienneteWhenDateEmbaucheFuture(){
         //Given = Initialisation des donnée d'entrée
-        Employe theEmploye = new Employe();
-        theEmploye.setDateEmbauche(null);
+        LocalDate dateEmbauche = LocalDate.now().plusYears(2);
+        Employe theEmploye = new Employe("Doe","John","T12345", dateEmbauche, Entreprise.SALAIRE_BASE, 4 , 1d);
 
         //When
         Integer nbAnnesAnciennete = theEmploye.getNombreAnneeAnciennete();
 
         //Then = Vérification de ce que fait la méthode
         Assertions.assertThat(nbAnnesAnciennete).isZero();
+    }
+
+    //cas 2 : DateNow = 05/01/2022 | dateEmbauche = 05/01/2022 | nbAnneeAnciennete : 0
+    @Test
+    public void testGetNombreAnneeAncienneteWhenDateEmbaucheNow(){
+        //Given = Initialisation des donnée d'entrée
+        LocalDate dateEmbauche = LocalDate.now();
+        Employe theEmploye = new Employe("Doe","John","T12345", dateEmbauche, Entreprise.SALAIRE_BASE, 4 , 1d);
+
+        //When
+        Integer nbAnnesAnciennete = theEmploye.getNombreAnneeAnciennete();
+
+        //Then = Vérification de ce que fait la méthode
+        Assertions.assertThat(nbAnnesAnciennete).isZero();
+    }
+
+
+    //cas 3 : DateNow = 05/01/2022 | dateEmbauche = 05/01/2021 | nbAnneeAnciennete : 1
+    @Test
+    public void testGetNombreAnneeAncienneteWhenDateEmbauchePasse(){
+        //Given = Initialisation des donnée d'entrée
+        LocalDate dateEmbauche = LocalDate.now().minusYears(2);
+        Employe theEmploye = new Employe("Doe","John","T12345", dateEmbauche, Entreprise.SALAIRE_BASE, 4 , 1d);
+
+        //When
+        Integer nbAnnesAnciennete = theEmploye.getNombreAnneeAnciennete();
+
+        //Then = Vérification de ce que fait la méthode
+        Assertions.assertThat(nbAnnesAnciennete).isZero();
+    }
+
+    //cas 4 : DateNow = now | dateEmbauche = null | nbAnneeAnciennete : 0
+    @Test
+    public void testGetNbAnneeAncienneteWithDateEmbaucheNull(){
+        //Given
+        LocalDate dateEmbauche = null;
+
+        Employe employe = new Employe("Doe", "John", "T12345", dateEmbauche, Entreprise.SALAIRE_BASE, 4, 1d);
+        //When
+        Integer nbAnneeAnciennete = employe.getNombreAnneeAnciennete();
+        //Then
+        Assertions.assertThat(nbAnneeAnciennete).isZero();
     }
 }
